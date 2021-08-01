0b9524602911062b9caae66be9c75928fa4cacbd
Ricardo Damasceno <ricardodamascenno@gmail.com>
2021-08-01 16:24:37 -0300

    Innitial commit

---
+++ src/main/kotlin/com/spring/aws/SpringAwsApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.aws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAwsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAwsApplication>(*args)
+}
---
+++ src/test/kotlin/com/spring/aws/SpringAwsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.spring.aws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAwsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
